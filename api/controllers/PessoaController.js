0af8433929ef2285ffe1d5172a15a9e0a039648a
lmoraesdev <lbc_moraes@outlook.com>
2022-12-16 21:37:56 -0300

    feat: routers

--- api/controllers/PessoaController.js
+++ api/controllers/PessoaController.js
@@ -9,6 +9,42 @@ class PessoasController {
 			return res.status(500).json(error.message);
 		}
 	}
+
+	static async pegaUmPessoa(req, res) {
+		const { id } = req.params;
+		try {
+			const umaPessoa = await database.Pessoas.findOne({
+				where: { id: Number(id) },
+			});
+			return res.status(200).json(umaPessoa);
+		} catch (error) {
+			return res.status(500).json(error.message);
+		}
+	}
+
+	static async criaPessoa(req, res) {
+		const novaPessoa = req.body;
+		try {
+			const novaPessoaCriada = await database.Pessoas.create(novaPessoa);
+			return res.status(200).json(novaPessoaCriada);
+		} catch {
+			return res.status(500).json(error.message);
+		}
+	}
+
+	static async atualizaPessoa(req, res) {
+		const { id } = req.params;
+		const novasInfos = req.body;
+		try {
+			await database.Pessoas.update(novasInfos, { where: { id: Number(id) } });
+			const pessoasAtualizada = await database.Pessoas.findOne({
+				where: { id: Number(id) }
+			})
+			return res.status(200).json(pessoasAtualizada)
+		} catch {
+			return res.status(500).json(error.message);
+		}
+	}
 }
 
 module.exports = PessoasController;
--- api/routes/pessoasRoute.js
+++ api/routes/pessoasRoute.js
@@ -1,8 +1,12 @@
 const { Router } = require("express");
+const { pegaTodasAsPessoas } = require("../controllers/PessoaController");
 const PessoasController = require("../controllers/PessoaController");
 
 const router = Router();
 
 router.get("/pessoas", PessoasController.pegaTodasAsPessoas);
+router.get("/pessoas/:id", PessoasController.pegaUmPessoa);
+router.post("/pessoas", PessoasController.criaPessoa);
+router.put("/pessoas/:id", PessoasController.atualizaPessoa)
 
-module.exports = router
+module.exports = router;
