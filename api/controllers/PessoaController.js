808b5347308d9c1ecc9833af7f43b8423dfa11fc
lmoraesdev <lbc_moraes@outlook.com>
2022-12-21 13:52:55 -0300

    feat: rotas de restaurar

--- api/controllers/PessoaController.js
+++ api/controllers/PessoaController.js
@@ -59,6 +59,20 @@ class PessoasController {
 		}
 	}
 
+	static async restauraPessoa(req, res) {
+		const { id } = req.params;
+		try {
+			await database.Pessoas.restore({
+				where: { id: Number(id) },
+			});
+			return res.status(200).json({ message: `id ${id} restaurado` });
+		} catch {
+			return res.status(500).json(error.message);
+		}
+	}
+
+	//matriculas
+
 	static async pegaUmaMatricula(req, res) {
 		const { estudanteId, matriculaId } = req.params;
 		try {
@@ -110,6 +124,21 @@ class PessoasController {
 				where: { id: Number(matriculaId) },
 			});
 			return res.status(200).json({ message: `id ${matriculaId} deletado` });
+		} catch {
+			return res.status(500).json(error.message);
+		}
+	}
+
+	static async restauraMatricula(req, res) {
+		const { estudanteId, matriculaId } = req.params;
+		try {
+			await database.Matriculas.restore({
+				where: {
+					id: Number(matriculaId),
+					estudanteId: Number(estudanteId),
+				},
+			});
+			return res.status(200).json({ message: `id ${matriculaId} restaurado` });
 		} catch {
 			return res.status(500).json(error.message);
 		}
--- api/routes/pessoasRoute.js
+++ api/routes/pessoasRoute.js
@@ -2,24 +2,29 @@ const { Router } = require("express");
 const PessoasController = require("../controllers/PessoaController");
 
 const router = Router();
-
-router.get("/pessoas", PessoasController.pegaTodasAsPessoas);
-router.get("/pessoas/:id", PessoasController.pegaUmPessoa);
-router.post("/pessoas", PessoasController.criaPessoa);
-router.put("/pessoas/:id", PessoasController.atualizaPessoa);
-router.delete("/pessoas/:id", PessoasController.apagaPessoa);
-router.get(
-	"/pessoas/:estudanteId/matricula/:matriculaId",
-	PessoasController.pegaUmaMatricula
-);
-router.post("/pessoas/:estudanteId/matricula", PessoasController.criaMatricula);
-router.put(
-	"/pessoas/:estudanteId/matricula/:matriculaId",
-	PessoasController.atualizaMatricula
-);
-router.delete(
-	"/pessoas/:estudanteId/matricula/:matriculaId",
-	PessoasController.apagaMatricula
-);
+router
+	.get("/pessoas", PessoasController.pegaTodasAsPessoas)
+	.get("/pessoas/:id", PessoasController.pegaUmPessoa)
+	.post("/pessoas", PessoasController.criaPessoa)
+	.put("/pessoas/:id", PessoasController.atualizaPessoa)
+	.delete("/pessoas/:id", PessoasController.apagaPessoa)
+	.get(
+		"/pessoas/:estudanteId/matricula/:matriculaId",
+		PessoasController.pegaUmaMatricula
+	)
+	.post("/pessoas/:estudanteId/matricula", PessoasController.criaMatricula)
+	.post("/pessoas/:id/matricula", PessoasController.restauraPessoa)
+	.post(
+		"/pessoas/:estudanteId/matricula/:matriculaId",
+		PessoasController.restauraMatricula
+	)
+	.put(
+		"/pessoas/:estudanteId/matricula/:matriculaId",
+		PessoasController.atualizaMatricula
+	)
+	.delete(
+		"/pessoas/:estudanteId/matricula/:matriculaId",
+		PessoasController.apagaMatricula
+	);
 
 module.exports = router;
